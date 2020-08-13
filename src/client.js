d3d7c287bc6d03777327f5a893a1ba2179f07d41
Kapehe <sarahkapehe@gmail.com>
2020-08-13 14:33:54 -0700

    removing projectid

--- src/client.js
+++ src/client.js
@@ -1,7 +1,7 @@
 import sanityClient from "@sanity/client";
 
 export default sanityClient({
-  projectId: "us3msdvj", // find this at manage.sanity.io
+  projectId: "YOUR PROJECT ID HERE", // find this at manage.sanity.io, run `sanity init` to initialize
   dataset: "production", // this is from when we answered those question from 'sanity init'
   useCdn: true,
 });
